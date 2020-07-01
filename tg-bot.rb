5d128a68532528617e6da031cf03ea1133936a72
ioilmio <illuminato.salvatore@gmail.com>
2020-07-01 23:38:23 +0200

    moved logic into case method...beginner mistakes solved

--- tg-bot.rb
+++ tg-bot.rb
@@ -8,32 +8,28 @@ require 'faraday'
 Dotenv.load('.bot_key.env')
 url = 'https://programming-quotes-api.herokuapp.com/quotes'
 
-response = Faraday.get(url)
-# p response
-data_hash = JSON.parse(response.body)
-# p data_hash.class
-random_quotes = data_hash.sample
-p random_quotes
-p quote = random_quotes['en']
-p author = random_quotes['author']
-# quote = data_hash['en']
-# author = data_hash['author']
-
 bot = TelegramBot.new(token: ENV['BOT_KEY'])
+
 bot.get_updates(fail_silently: true) do |message|
-  # puts "@#{message.from.username}: #{message.text}"
+  puts "@#{message.from.username}: #{message.text}"
   command = message.get_command_for(bot)
 
   message.reply do |reply|
     reply.text = case command
-        when /start/i
-          'This bot will give you random quotes from famous programmers and computer scientists'
-        when /quotes/i
-          "#{quote} from #{author}!"
-        else
-          "#{message.from.first_name}, have no idea what #{command.inspect} means."
+                 when /start/i
+                   'This bot will give you random quotes from famous programmers and computer scientists'
+                 when /quotes/i
+                   url = 'https://programming-quotes-api.herokuapp.com/quotes'
+                   response = Faraday.get(url)
+                   data_hash = JSON.parse(response.body)
+                   random_quotes = data_hash.sample
+                   quote = random_quotes['en']
+                   author = random_quotes['author']
+                   "#{quote} from #{author}!"
+                 else
+                   "#{message.from.first_name}, have no idea what #{command.inspect} means."
         end
-    # puts "sending #{reply.text.inspect} to @#{message.from.username}"
+    puts "sending #{reply.text.inspect} to @#{message.from.username}"
     reply.send_with(bot)
   end
 end
